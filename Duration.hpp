dae9d90b6bb17ded37feb224581524ef0b87a671
WizzOFF <maher_off@proton.me>
2025-04-12 17:32:18 +0200

    Duration.hpp: create class Duree

---
+++ Duration.hpp
@@ -0,0 +1,18 @@
+
+#ifndef DEF_DUREE
+#define DEF_DUREE
+ 
+class Duree
+{
+    public:
+ 
+    Duree(int heures = 0, int minutes = 0, int secondes = 0);
+ 
+    private:
+ 
+    int m_heures;
+    int m_minutes;
+    int m_secondes;
+};
+ 
+#endif
